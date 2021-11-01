ce8ab3be74090e6f18c9727962ca2313b017e919
Kamil Foltynski <kamil@foltynski.com>
2021-11-01 15:41:33 +0100

    Added example funciton

---
+++ R/run_example.R
@@ -0,0 +1,20 @@
+#' run_example
+#'
+#' Run exapmle shiny app
+#'
+#' @param example_dir example name
+#'
+#' @export
+#'
+run_example <- function(example_dir) {
+  stopifnot(nzchar(example_dir))
+
+  available_examples <-  list.files(system.file("examples", package = "tryCatchShiny"))
+
+  if (!example_dir %in% available_examples)
+    stop("Available examples are: ", paste0(available_examples, collapse = ", "))
+
+  app_dir <- system.file("examples", example_dir,  package = "tryCatchShiny")
+
+  shiny::runApp(app_dir, display.mode = "normal")
+}
